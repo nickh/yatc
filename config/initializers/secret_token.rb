19bf45eadcd44b27db7a43174b31540d8f3f5dd9
Nick Hengeveld <nickh@hengeveld.com>
2010-08-15 20:02:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yatc::Application.config.secret_token = '5496673a45481c880fdbefa080640d1689b0cb982ee33740ae68bc4d80ee74236494ed50343abe9d1f8362f7ac7284ae5da0404e397a84f6d51276fd1494762b'
