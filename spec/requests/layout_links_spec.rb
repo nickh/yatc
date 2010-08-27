390ef0c58597c18122efde49677877d62b29284d
Nick Hengeveld <nickh@hengeveld.com>
2010-08-26 21:10:47 -0700

    Refactor layout and routes

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  it "has a Home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => 'Home')
+  end
+
+  it "has a Contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => 'Contact')
+  end
+
+  it "has a About page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => 'About')
+  end
+
+  it "has a Help page at '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => 'Help')
+  end
+end
