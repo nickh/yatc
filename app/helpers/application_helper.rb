975884787f47510fa11fa8eacaa080a1a93f1ffa
Nick Hengeveld <nickh@hengeveld.com>
2010-08-27 21:49:35 -0700

    Refactored app layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,8 @@ module ApplicationHelper
     base_title = 'Ruby on Rails Tutorial Sample App'
     @title.nil?? base_title : "#{base_title} | #{@title}"
   end
+
+  def logo
+    image_tag('logo.png', :alt => "Sample App", :class => "round")
+  end
 end
